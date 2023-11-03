a31ac987a334fe6c69212cff9f32674486b9c261
aaronbraganca2 <144013061+aaronbraganca2@users.noreply.github.com>
2023-11-03 23:52:17 +0530

    Add files via upload

---
+++ User/User.Master.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace BussinEats.User
+{
+    public partial class User : System.Web.UI.MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if(!Request.Url.AbsoluteUri.ToString().Contains("Default.aspx"))
+            {
+                form1.Attributes.Add("class", "sub_page");
+            }
+            else
+            {
+                form1.Attributes.Remove("class");
+                //load the control
+                Control sliderUserControl = (Control)Page.LoadControl("SliderUserControl.ascx");
+
+                pnlSliderUC.Controls.Add(sliderUserControl);
+            }
+        }
+    }
+}
